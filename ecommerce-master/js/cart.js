4e62c90f45c8fdceacda00c270e85d2cb371bebf
Romina-Almi <54241780+Romina-Almi@users.noreply.github.com>
2019-10-17 23:06:37 -0300

    Pautas del 3er entregable
    Se realiza una petición a una URL, con producto precargado.Se muestra en HTML la información del mismo y sus elementos gráficos que involucran un carrito.
    Se refleja el costo total de la compra del  articulo en tiempo real modificando el cantidad.

--- ecommerce-master/js/cart.js
+++ ecommerce-master/js/cart.js
@@ -10,10 +10,24 @@ let ERROR_MSG = "Ha habido un error :(, verifica qué pasó.";
 
 //Función que se utiliza para actualizar los costos de publicación
 function updateTotalCosts(){
+  let shippingCostHTML = document.getElementById("shippingCostText");
+  let shippingToShow = product.currency + Math.round((shippingPercentage * 100));
+
+  shippingCostHTML.innerHTML = shippingToShow;
+
+
+  total = product.currency +  Math.round(((product.unitCost * cantidad.value) * shippingPercentage * 100) / 100);
+  document.getElementById('totalCostText').innerHTML= total;
+
 
 }
 
 function updateSubtotal(){
+  subtotal= product.currency + product.unitCost * cantidad.value;
+  document.getElementById("subtotal").innerHTML=subtotal;
+
+  subtotal= product.currency + product.unitCost * cantidad.value;
+  document.getElementById("subtotalCostText").innerHTML=subtotal;
 
 }
 
@@ -26,6 +40,29 @@ function hidePaymentTypeNotSelected(){
 }
 
 function showArticles(articles){
+  let imgid = document.getElementById('img');
+  let img = '<img src="'+ product.src+'" class="img-fluid img-thumbnaail">';
+  let name = document.getElementById('name');
+  let precio = document.getElementById("precio");
+  let cantidad = document.getElementById("cantidad");
+
+  imgid.innerHTML = img;
+  name.innerHTML = product.name
+  precio.innerHTML =  product.currency + product.unitCost;
+  cantidad.value = product.count;
+  updateSubtotal();
+  updateTotalCosts();
+
+  cantidad.onclick = function(){
+    updateSubtotal();
+    updateTotalCosts();
+  }
+  cantidad.onkeyup = function(){
+    updateSubtotal();
+    updateTotalCosts();
+  }
+
+
 
 }
 
@@ -33,5 +70,27 @@ function showArticles(articles){
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(CART_INFO_URL).then(function(response){
+        product = response.data.articles[0];
+     showProducts(product); 
+   });
+
+
+   document.getElementById("Premiumradio").addEventListener("change", function(){
+    shippingPercentage = 0.15;
+    updateTotalCosts();
+  });
+
+  document.getElementById("Expressradio").addEventListener("change", function(){
+    shippingPercentage = 0.7;
+    updateTotalCosts();
+  });
+
+  document.getElementById("Standardradio").addEventListener("change", function(){
+    shippingPercentage = 0.5;
+    updateTotalCosts();
+  });
+
+ 
 
 });
